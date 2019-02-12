aee43e33efa904c07045bf315f5e3019ad46253a
Pilmo Kang <ivorycirrus@gmail.com>
2019-02-12 20:54:35 +0900

    solve : basic.step1.ex08

--- JavaExercises/src/basic/step1/ex08/Main.java
+++ JavaExercises/src/basic/step1/ex08/Main.java
@@ -12,7 +12,28 @@ public class Main {
 		//---------------------------------------
 		// TODO : Write code below
 		
-		System.out.println("*");
+		for(int i = 1 ; i <= N ; i++) {
+			String line = "";
+			
+			// Insert white-space before left heading
+			int indent = N-i;
+			for(int j = 0 ; j < indent ; j++) line+=" ";
+			
+			// Mark stars
+			int stars = 2*i-1;
+			for(int j = 0 ; j < stars ; j++) {
+				if(j==0 || j == stars-1 || i == N) {
+					// edge
+					line+="*";
+				} else {
+					// inside of triangle
+					line+=" ";
+				}
+			}
+			
+			// Print
+			System.out.println(line);
+		}
 		
 		//---------------------------------------
 	}
