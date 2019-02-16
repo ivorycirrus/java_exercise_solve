162b901019d2a126c8317694ac659b8aeea50d76
ivorycirrus <ivorycirrus@ivorycirrus-desktop>
2019-02-16 19:11:22 +0900

    solve : basic.step3.ex01

--- JavaExercises/src/basic/step3/ex01/Main.java
+++ JavaExercises/src/basic/step3/ex01/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		int N=1; // Num of players
+		int N=4; // Num of players
 		int S=1; // Seat
 
 		call(N, S);
@@ -14,8 +14,27 @@ public class Main {
 		//---------------------------------------
 		// TODO : Write code below
 		
-		for(int i = 0 ; i < 5 ; i++) {			
-			System.out.println(i);
+		for(int i = 0 ; i < 5 ; i++) {
+			// call number at this turn
+			int num = i * N + S;
+			
+			// find 3,6,9 from call number
+			String X = "";
+			for(int j = num ; j > 0 ; j /= 10) {
+				// get one number by tail
+				int tail = num%10;
+				// check 3,6,9
+				if(tail%3 == 0) X += "X";
+			}
+			
+			// call number
+			if("".contentEquals(X)){
+				// 3,6,9 is not found
+				System.out.println(num);
+			} else {
+				// call 'X' by 3,6,9
+				System.out.println(X);
+			}
 		}
 		
 		//---------------------------------------
